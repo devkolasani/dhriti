bde8460b5aeb0263e2dcaaeb3756814b21171451
Krishna Vivek Kolasani <vivekkolasani45@gmail.com>
2020-11-15 22:59:05 +0530

    cloudsync music removed

--- scripts/cloudsync/cloudsync.sh
+++ scripts/cloudsync/cloudsync.sh
@@ -3,11 +3,7 @@
 # Sync Documents local dir with Google Drive personal
 rclone sync -P /mnt/storage/Documents GoogleDrivePersonal:/Documents
 
-# Sync Music local Dir with One Drive Personal
-rclone sync -P /mnt/storage/Music OneDrivePersonal:/Music
-
-# Backup Pictures, Documents and Music to One Drive Work
-rclone sync -P /mnt/storage/Music OneDriveWork:/Music
+# Backup Pictures, Documents and Videos to One Drive Work
 rclone sync -P /mnt/storage/Videos/Courses OneDriveWork:/Videos/Courses
 rclone sync -P /mnt/storage/Videos/Cricket OneDriveWork:/Videos/Cricket
 rclone sync -P /mnt/storage/Videos/Walls OneDriveWork:/Videos/Walls
